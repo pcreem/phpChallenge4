8276170dfed329db23f24ff75b29e73028e96268
pcreem <creempepper@gmail.com>
2021-03-26 08:40:04 +0800

    refactor: Simplify ConnectDB code
    Connect to database only when class construct
    Eliminate reducdant code in index.php
    Prepare for solving issue 0003 Singleton pattern DB

--- App/Core/Database/ConnectDB.php
+++ App/Core/Database/ConnectDB.php
@@ -1,23 +1,39 @@
 <?php
 namespace App\Core\Database;
+$dir = __DIR__.'/../../../';
+require_once $dir.'/vendor/autoload.php';
+use Dotenv\Dotenv;
 use PDO;
+
+$dotenv = Dotenv::createImmutable($dir);
+$dotenv->load();
 
 class ConnectDB {
   private static $instance = null;
-  private $conn;
+  private $conn; 
+
+  private $host = '';
+  private $dbname = '';
+  private $user = '' ;
+  private $password = '';
   
-  private function __construct($host, $user, $pass, $name)
+  private function __construct()
   {
-    $this->conn = new PDO("mysql:host={$host};
-    dbname={$name}", $user,$pass,
+    $this->host = $_ENV['DB_HOST'];
+    $this->user = $_ENV['DB_USERNAME'];
+    $this->password = $_ENV['DB_PASSWORD']; 
+    $this->dbname = $_ENV['DB_DATABASE'];
+
+    $this->conn = new PDO("mysql:host={$this->host};
+    dbname={$this->dbname}", $this->user,$this->password,
     array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
   }
   
-  public static function getInstance($host, $user, $pass, $name)
+  public static function getInstance()
   {
     if(!self::$instance)
     {
-      self::$instance = new ConnectDB($host, $user, $pass, $name);
+      self::$instance = new ConnectDB();
     }
     return self::$instance;
   }
--- index.php
+++ index.php
@@ -4,22 +4,8 @@ require_once __DIR__.'/vendor/autoload.php';
 
 use App\Controller\DatabaseController;
 use App\Core\Database\ConnectDB;
-use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-$host = $_ENV['DB_HOST'];
-$dbname   = $_ENV['DB_DATABASE'];
-$user = $_ENV['DB_USERNAME'];
-$password = $_ENV['DB_PASSWORD'];
-$port = $_ENV['DB_PORT'];
-$charset = 'utf8mb4';
-
-$dsn = "mysql:host=$host;dbname=$dbname;charset=$charset;port=$port";
-$filepath = $_ENV['FILE_PATH'];
-
-$pdo = ConnectDB::getInstance($host, $user, $password, $dbname)->getConnection();
+$pdo = ConnectDB::getInstance()->getConnection();
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
